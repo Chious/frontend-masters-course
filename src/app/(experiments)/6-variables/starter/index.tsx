5714a8bef1d44680233a6a516baec87066850965
chious <chu23294752@gmail.com>
2026-05-28 15:17:48 +0800

    feat. 滑鼠事件追蹤
    透過 useRef 以及 --distance 控制字體動畫

--- src/app/(experiments)/6-variables/starter/index.tsx
+++ src/app/(experiments)/6-variables/starter/index.tsx
@@ -2,19 +2,69 @@
 
 import { cn } from "@/lib/utils";
 import s from "./styles.module.css";
+import { useEffect, useRef, useState } from "react";
+import { distance } from "@/lib/math";
+import { Roboto_Flex } from "next/font/google";
+import Script from "next/script";
+
+const roboto = Roboto_Flex({ subsets: ["latin"] });
 
 export default function Page() {
+  const [distanceValue, setDistanceValue] = useState(0);
+  const titleRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    window.addEventListener(
+      "mousemove",
+      (event) => {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        const screenW = window.innerWidth;
+        const screenH = window.innerHeight;
+
+        const centerX = screenW / 2;
+        const centerY = screenH / 2;
+
+        const d = distance(mouseX, mouseY, centerX, centerY);
+        const maxDistance = distance(0, 0, centerX, centerY);
+
+        if (titleRef.current) {
+          titleRef.current.style.setProperty(
+            "--distance",
+            String(d / maxDistance),
+          );
+        }
+      },
+      {
+        signal: controller.signal,
+      },
+    );
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return (
     <div
       className={cn(
         "w-screen h-screen text-white flex items-center justify-center",
-        s.grid
+        s.grid,
       )}
     >
+      <Script
+        crossOrigin="anonymous"
+        src="//unpkg.com/react-scan/dist/auto.global.js"
+      />
       <h1
+        ref={titleRef}
         className={cn(
           "uppercase text-[10vh] leading-none relative",
-          s["title"]
+          s["title"],
+          roboto.className,
         )}
       >
         Variables
