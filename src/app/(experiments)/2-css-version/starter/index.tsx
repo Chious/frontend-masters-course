52532a89db1e279870a63dec552a3b7e2a96e3f0
chious <chu23294752@gmail.com>
2026-05-28 14:37:55 +0800

    feat: practice section 2
    CLS 動畫
    
    處理 hero section 時，透過 CSS 產生動畫而非 Javascript ，讓使用者可以更快與畫面互動。
    
    - animation-fill-mode：用來設定初始畫面，避免 CLS

--- src/app/(experiments)/2-css-version/starter/index.tsx
+++ src/app/(experiments)/2-css-version/starter/index.tsx
@@ -1,11 +1,41 @@
+import style from "./style.module.css";
+
+const TEXT = ["CSS".split(""), "version".split("")];
+
 export default function Page() {
   return (
     <div className="bg-blue-300 text-black">
       <div className="flex h-screen items-end justify-left overflow-hidden">
         <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
-          CSS
+          {TEXT[0].map((letter, i) => (
+            <span
+              key={i}
+              className={style.letter}
+              style={
+                {
+                  "--index": i,
+                } as React.CSSProperties
+              }
+            >
+              {letter}
+            </span>
+          ))}
           <br />
-          version
+
+          {TEXT[1].map((letter, i) => (
+            <span
+              key={i}
+              className={style.letter}
+              style={
+                {
+                  "--index": i,
+                } as React.CSSProperties
+              }
+            >
+              {letter}
+            </span>
+          ))}
+          <br />
         </h1>
       </div>
     </div>
